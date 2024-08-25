de46f29daa8293ca8be2d768f9ed287b8fc56407
Alessandro Marotta <alessand.marotta@gmail.com>
2024-08-25 23:32:48 +0200

    Fix dimensioni sottotesto nella home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ export default function Home() {
   return (
     <Box  width="100%" maxHeight="100vh">
       <Flex justify="center" align="center" direction="column"  minWidth="100vw" minHeight="80vh">
-        <Heading size={{
-          initial: "7", xs : "7", xl: "9"
-          }}>
+        <Heading size={{initial: "7", xs : "7", xl: "9"}}>
             Alessandro Marotta
         </Heading>
-        <Text size={{initial: "7"}}> Sviluppatore Backend</Text>
+        <Text size={{initial: "5", xs: "5", xl: "7"}}> Sviluppatore Backend</Text>
       </Flex>
     </Box>
   );
